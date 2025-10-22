30a0fbcd9c52200e1af0db669eda5a774143b5b2
Sukhchandan Randhawa <sukhchandan.95@example.com>
2025-10-22 14:58:57 -0700

    Updated CalculatorTest.java to make it fail

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -6,7 +6,7 @@ public class CalculatorTest {
     @Test
     void testAdd() {
         Calculator calc = new Calculator();
-        assertEquals(5, calc.add(2, 3));
+        assertEquals(6, calc.add(2, 3));
     }
 
     @Test
@@ -24,7 +24,7 @@ public class CalculatorTest {
     @Test
     void testDivide() {
         Calculator calc = new Calculator();
-        assertEquals(2, calc.divide(6, 3));
+        assertEquals(1, calc.divide(6, 3));
     }
 
     @Test
